bd4779fb33e3d022ef71737b5b29981e50842e3b
inlovewithanvii <kajalkaraditya247@gmail.com>
2026-02-13 23:48:41 +0530

    script.js

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const noBtn = document.getElementById("noBtn");
 const yesBtn = document.getElementById("yesBtn");
 
 noBtn.addEventListener("mouseover", () => {
-  const x = Math.random() * (window.innerWidth - 100);
-  const y = Math.random() * (window.innerHeight - 50);
+ const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
+const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
   noBtn.style.position = "absolute";
   noBtn.style.left = x + "px";
   noBtn.style.top = y + "px";
 });
 
 yesBtn.addEventListener("click", () => {
-  document.body.innerHTML = "<h1 style='text-align:center;margin-top:20%;color:#ff2e63;'>YAYYY ❤️ I KNEW IT 😍</h1>";
-});
+  document.getElementById("container").innerHTML =
+"<h1 style='text-align:center;margin-top:20%;color:#ff2e63;'>YAYYY 💗 I KNEW IT 😍</h1>";
+;
