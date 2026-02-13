111c54a972b2f58d7e0c04e22561a01e7fe3f55b
inlovewithanvii <kajalkaraditya247@gmail.com>
2026-02-13 23:56:03 +0530

    script.js

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ yesBtn.addEventListener("click", () => {
   document.getElementById("container").innerHTML =
 "<h1 style='text-align:center;margin-top:20%;color:#ff2e63;'>YAYYY 💗 I KNEW IT 😍</h1>";
 ;
+const music = document.getElementById("bg-music");
+
+function toggleMusic() {
+  if (music.paused) {
+    music.play();
+  } else {
+    music.pause();
+  }
+}
+document.addEventListener("click", () => {
+  if (music.paused) music.play();
+}, { once: true });
