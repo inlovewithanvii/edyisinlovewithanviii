59a700666c66dfb83f1b2478859344385b055d92
inlovewithanvii <kajalkaraditya247@gmail.com>
2026-02-14 00:00:23 +0530

    script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const noBtn = document.getElementById("no-btn");
 const yesBtn = document.getElementById("yes-btn");
 const music = document.getElementById("bg-music");
 
+// Move No button
 noBtn.addEventListener("mouseover", () => {
   const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
   const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
@@ -10,11 +11,13 @@ noBtn.addEventListener("mouseover", () => {
   noBtn.style.top = y + "px";
 });
 
+// Yes click
 yesBtn.addEventListener("click", () => {
   document.querySelector(".container").innerHTML =
     "<h1 style='text-align:center;margin-top:20%;color:#ff2e63;'>YAYYY 💗 I KNEW IT 😍</h1>";
 });
 
+// Music toggle
 function toggleMusic() {
   if (music.paused) {
     music.play();
@@ -23,6 +26,7 @@ function toggleMusic() {
   }
 }
 
+// Start music on first click
 document.addEventListener("click", () => {
   if (music.paused) music.play();
 }, { once: true });
